ddb5559ad68a40d2b66d0d0f264275b997e3ee5b
sara <saralundkvist86@gmail.com>
2020-09-01 20:44:27 +0200

    writing 3 test for 3projects respectively in test 'userCanSeeListOfProjects'

--- cypress/integration/userCanSeeListOfProjects.feature.js
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -1,6 +1,34 @@
-describe("User can see list of projects" () => {
+describe("User can see list of projects", () => {
     beforeEach(() => {
         cy.visit("http://localhost:3000");
         cy.get("#projects-tab").click();
-    }
+    });
+
+    it("displays first project", () => {
+        cy.get("#project-1").within(() => {
+            cy.get(".image").should("exist");
+            cy.get(".ui.header").should("contain", "My First Website");
+            cy.get(".description").should("contain", "This was my first project");
+
+        });
+    });
+
+    it("displays second project", () => {
+        cy.get("#project-2").within(() => {
+        cy.get(".image").should("exist");
+        cy.get(".ui.header").should("contain", "UI Design");
+        cy.get(".description").should("contain", "Designins user interfaces is fun");
+    });
 });
+
+    it("displays third project", () => {
+        cy.get("#project-3").within(() => {
+            cy.get(".image").should("exist");
+            cy.get(".ui.header").should("contain", "Mobile UX");
+            cy.get(".description").should("contain", "I like to design for the mobile platform");
+
+        });
+    });
+});
+
+
