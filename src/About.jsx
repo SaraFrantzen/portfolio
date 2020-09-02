e447a555f92e258cad0b99e5d5227322689bbec3
sara <saralundkvist86@gmail.com>
2020-09-02 15:59:55 +0200

    got help from Adi to install react-undraw-illustrations, had to expand as im using Linux. Adding illustrations to Hello.jsx and About.jsx using react-undraw-illustrations

--- src/About.jsx
+++ src/About.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-//import { UndrawFreelancer } from "react-undraw-illustrations";
+import { UndrawFreelancer } from "react-undraw-illustrations";
 const About = () => {
   return (
     <div className="ui main container">
       <div class="ui stackable two column grid">
         <div class="column">
-          {/* <UndrawFreelancer /> */}
+           <UndrawFreelancer /> 
         </div>
         <div class="column">
           <h1 className="ui header">About Me</h1>
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+import UndrawMeditation from "react-undraw-illustrations/lib/components/UndrawMeditation";
 const Hello = () => {
   return (
+
     <div className="ui main container">
-      <h1 id="hello">Hello World</h1>
+      <h1 id="hello">Sara Lundkvist</h1>
+      <div class="column">
+     <UndrawMeditation /> 
+  </div>
     </div>
   );
 };
