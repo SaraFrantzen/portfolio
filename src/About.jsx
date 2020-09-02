89f07ad5f6bc6ff78c48ed9b23162dba22fe19de
sara <saralundkvist86@gmail.com>
2020-09-02 13:28:14 +0200

    trying to add illustrations using undraw illustrations by react, but localhost crash as i do so. Coach Emma is looking into the problem. Removed React-undraw-illustrations in the meanwhile

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+//import { UndrawFreelancer } from "react-undraw-illustrations";
 const About = () => {
   return (
     <div className="ui main container">
-      <h1 id="about-header" className="ui header">
-        About Me
-      </h1>
-      <p>My name is Sara and I attend the Craft Academy BootCamp</p>
+      <div class="ui stackable two column grid">
+        <div class="column">
+          {/* <UndrawFreelancer /> */}
+        </div>
+        <div class="column">
+          <h1 className="ui header">About Me</h1>
+          <p>My name is Sara and I attend the Craft Academy BootCamp</p>
+        </div>
+      </div>
     </div>
   );
 };
