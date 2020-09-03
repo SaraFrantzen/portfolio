619d5420c6961bf7c8eeb95d3146fa6751818111
sara <saralundkvist86@gmail.com>
2020-09-03 18:05:56 +0200

    changing 'About Me' section into a CV section, changing all id's etc to replace about with cv

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Hello from "./Hello";
 import Header from "./Header";
 import Footer from "./Footer";
 import Projects from "./Projects";
-import About from "./About";
+import Cv from "./Cv";
 import Contact from "./Contact";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
@@ -14,7 +14,7 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path="/" component={Hello}></Route>
-        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/cv" component={Cv}></Route>
         <Route exact path="/projects" component={Projects}></Route>
         <Route exact path="/contact" component={Contact}></Route>
       </Switch>
